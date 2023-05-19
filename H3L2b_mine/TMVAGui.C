4eeb885f549165254ef89d8084385e2202da4cdb
JiYuanjing <jiyj@mail.ustc.edu.cn>
2023-05-18 23:18:27 -0400

    tmva

---
+++ H3L2b_mine/TMVAGui.C
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+#include "TList.h"
+#include "TROOT.h"
+#include "TKey.h"
+#include "TString.h"
+#include "TControlBar.h"
+#include "TObjString.h"
+#include "TClass.h"
+
+//#include "tmvaglob.C"
+
+// some global lists
+
+{
+	TMVA::TMVAGui(" TMVA_H3L2b.root" );
+}
---
+++ H3L2b_mine/app.sh
@@ -0,0 +1,1 @@
+root -l ./TMVAClassificationApplication.C\(\"Cuts\"\)
